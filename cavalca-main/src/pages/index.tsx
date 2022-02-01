2b4f2061759c16ce905aa6242f915809a531e4f2
cavalcaconstrucoes <98845385+cavalcaconstrucoes@users.noreply.github.com>
2022-02-01 15:49:13 -0400

    Add files via upload

---
+++ cavalca-main/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import RentingGrid from '../components/RentingGrid';
+import Footer from '../components/Footer';
+
+const Home: React.FC = () => {
+  return (
+      <>
+     <RentingGrid />
+     <Footer />
+     </>
+  )
+}
+
+export default Home
