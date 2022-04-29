e70501476e8a5ed59b1c78714fa433ff7e539a4f
cavalcaconstrucoes <administrador@cavalca.com.br>
2022-04-29 13:43:25 -0400

    alteração

--- src/components/construcao/construcao.tsx
+++ src/components/construcao/construcao.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image'
 import constru from '../../images/em-construcao.png';
 
-const Construcao = (props) => {
+const Construcao = () => {
     return(
      <Image 
         src={constru}
